9517ad8123c173ff12080de765ea88bc48778070
CrypticSignal <hshafiq@hotmail.co.uk>
2021-07-18 16:38:07 +0100

    Misc.

--- content.js
+++ content.js
@@ -3,7 +3,33 @@
 // previewModal--player_container mini-modal has-smaller-buttons
 // videoMerchPlayer--boxart-wrapper
 
-async function getRatings(name, apiKey, ratingSite) {
+chrome.runtime.onMessage.addListener(() => {
+  console.log(namesScraped);
+});
+
+const getStorageData = (key) =>
+  new Promise((resolve, reject) =>
+    chrome.storage.sync.get(key, (object) =>
+      chrome.runtime.lastError
+        ? reject(Error(chrome.runtime.lastError.message))
+        : resolve(object[key])
+    )
+  );
+
+const getStorageValues = async () => {
+  apiKey = await getStorageData("apiKey");
+  ratingSite = await getStorageData("ratingSite");
+};
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", getStorageValues);
+} else {
+  getStorageValues();
+}
+
+let namesScraped = [];
+
+const getRatings = async (name, apiKey, ratingSite) => {
   const response = await fetch(`https://www.omdbapi.com/?t=${name}&apikey=${apiKey}`);
   const json = await response.json();
 
@@ -12,7 +38,7 @@ async function getRatings(name, apiKey, ratingSite) {
     return;
   }
 
-  console.log(json);
+  namesScraped.push(json);
 
   let totalScore = 0;
 
@@ -22,19 +48,16 @@ async function getRatings(name, apiKey, ratingSite) {
       // Metacritic
       if (score.includes("/100")) {
         metacritic = score;
-        console.log(`Metacritic: ${score}`);
         score = parseInt(score.slice(0, -4)) / 10;
 
         // IMDb
       } else if (score.includes("/10")) {
         imdb = score;
-        console.log(`IMDb: ${score}`);
         score = parseFloat(score.slice(0, -3));
 
         // Rotten Tomatoes
       } else if (score.includes("%")) {
         rottenTomatoes = score;
-        console.log(`Rotten Tomatoes: ${score}`);
         score = parseInt(score.slice(0, -1)) / 10;
       }
       totalScore += score;
@@ -43,14 +66,12 @@ async function getRatings(name, apiKey, ratingSite) {
   // Metacritic option chosen.
   else if (ratingSite === "metacritic") {
     if (!json.Ratings.some((e) => e.Source === "Metacritic")) {
-      console.log("No rating found on Metacritic.");
       return;
     }
     for (i = 0; i < json.Ratings.length; i++) {
       let score = json.Ratings[i].Value;
       if (score.includes("/100")) {
         metacritic = score;
-        console.log(`Metacritic: ${score}`);
         totalScore = parseInt(score.slice(0, -4)) / 10;
       }
     }
@@ -58,7 +79,6 @@ async function getRatings(name, apiKey, ratingSite) {
   // IMDb option chosen.
   else if (ratingSite === "imdb") {
     if (!json.Ratings.some((e) => e.Source === "Internet Movie Database")) {
-      console.log("No rating found on IMDb.");
       return;
     }
     for (i = 0; i < json.Ratings.length; i++) {
@@ -68,7 +88,6 @@ async function getRatings(name, apiKey, ratingSite) {
       }
       if (score.includes("/10")) {
         imdb = score;
-        console.log(`IMDb: ${score}`);
         totalScore = parseFloat(score.slice(0, -3));
       }
     }
@@ -76,79 +95,78 @@ async function getRatings(name, apiKey, ratingSite) {
   // Rotten Tomatoes option chosen.
   else if (ratingSite === "tomatoes") {
     if (!json.Ratings.some((e) => e.Source === "Rotten Tomatoes")) {
-      console.log("No rating found on Rotten Tomatoes.");
       return;
     }
     for (i = 0; i < json.Ratings.length; i++) {
       let score = json.Ratings[i].Value;
       if (score.includes("%")) {
         rottenTomatoes = score;
-        console.log(`Rotten Tomatoes: ${score}`);
         totalScore = parseInt(score.slice(0, -1)) / 10;
       }
     }
   }
 
   let averageRating = (totalScore / parseInt(json.Ratings.length)).toFixed(2);
-  if (ratingSite === "average") {
-    console.log(`Average = ${totalScore} / ${json.Ratings.length} = ${averageRating}`);
-  } else {
+  if (ratingSite !== "average") {
     averageRating = totalScore;
   }
 
   return averageRating;
-}
+};
 
-previousName = null;
-
-let movieName = "";
-let previousRatingSite = "";
+let doneNames = [];
 
 // Callback function to execute when mutations are observed
 const callback = async (mutationsList) => {
   // Use a traditional for-loop for IE 11
   for (const mutation of mutationsList) {
-    if (mutation.target.className === "previewModal--boxart") {
-      if (mutation.target.alt === previousName) {
+    if (mutation.attributeName === "data-ui-tracking-context") {
+      //const videoID = mutation.target.outerHTML.split(":")[7].split(",")[0];
+      const name = mutation.target.outerText;
+      if (name === "" || name.includes("\n")) {
+        return;
+      }
+      if (doneNames.some((element) => element === name)) {
         return;
       }
 
-      movieName = mutation.target.alt;
-      previousName = movieName;
+      doneNames.push(name);
 
-      const getStorageData = (key) =>
-        new Promise((resolve, reject) =>
-          chrome.storage.sync.get(key, (object) =>
-            chrome.runtime.lastError
-              ? reject(Error(chrome.runtime.lastError.message))
-              : resolve(object[key])
-          )
-        );
-
-      const apiKey = await getStorageData("apiKey");
-      const ratingSite = await getStorageData("ratingSite");
-      previousRatingSite = ratingSite;
-
-      const averageRating = await getRatings(movieName, apiKey, ratingSite);
+      const averageRating = await getRatings(name, apiKey, ratingSite);
 
       if (averageRating === undefined) {
+        const div = createDiv("N/A");
+        mutation.target.appendChild(div);
         return;
       }
 
-      const div = document.createElement("div");
-      div.style.width = "50px";
-      div.style.height = "50px";
-      div.style.display = "flex";
-      div.style.alignItems = "center";
-      div.style.justifyContent = "center";
-      div.style.borderRadius = "50%";
-      div.style.background = "red";
-      div.style.color = "white";
-      div.style.fontSize = "20px";
-      div.style.position = "relative";
-      div.innerHTML = averageRating;
+      const div = createDiv(averageRating);
+      mutation.target.appendChild(div);
+    } else if (mutation.target.className === "previewModal--boxart") {
+      const name = mutation.target.alt;
+      if (doneNames.some((element) => element === name)) {
+        return;
+      }
 
-      document.getElementsByClassName("videoMerchPlayer--boxart-wrapper")[0].appendChild(div);
+      doneNames.push(name);
+
+      const averageRating = await getRatings(name, apiKey, ratingSite);
+
+      if (averageRating === undefined) {
+        const div = createDiv("N/A");
+        document
+          .getElementsByClassName(
+            "previewModal--player_container mini-modal has-smaller-buttons"
+          )[0]
+          .appendChild(div);
+        return;
+      }
+
+      const div = createDiv(averageRating);
+
+      document
+        .getElementsByClassName("previewModal--player_container mini-modal has-smaller-buttons")[0]
+        .appendChild(div);
     }
   }
 };
@@ -156,9 +174,26 @@ const callback = async (mutationsList) => {
 const observer = new MutationObserver(callback);
 
 // Options for the observer (which mutations to observe)
-const config = { attributes: true, subtree: true };
+const config = { attributes: true, subtree: true, childList: true };
 
 const target = document.getElementsByClassName("netflix-sans-font-loaded")[0];
+//const target = document.getElementById("appMountPoint");
 
 // Start observing the target node for configured mutations
 observer.observe(target, config);
+
+const createDiv = (divText) => {
+  const div = document.createElement("div");
+  div.style.width = "50px";
+  div.style.height = "50px";
+  div.style.display = "flex";
+  div.style.alignItems = "center";
+  div.style.justifyContent = "center";
+  div.style.borderRadius = "50%";
+  div.style.background = "red";
+  div.style.color = "white";
+  div.style.fontSize = "20px";
+  div.style.position = "relative";
+  div.innerHTML = divText;
+  return div;
+};
--- options.js
+++ options.js
@@ -1,31 +1,39 @@
-document.addEventListener("DOMContentLoaded", restore_options);
-document.getElementById("save").addEventListener("click", save_options);
+document.addEventListener("DOMContentLoaded", () => {
+  restoreOptions();
+  document.getElementById("save").addEventListener("click", saveOptions);
+  document.getElementById("show-list").addEventListener("click", sendMessage);
+});
 
-function save_options() {
+function restoreOptions() {
+  chrome.storage.sync.get(function (items) {
+    items.apiKey
+      ? (document.getElementById("api-key").value = items.apiKey)
+      : (document.getElementById("api-key").value = "<Insert your OMDb API key>");
+    items.ratingSite
+      ? (document.getElementById("site").value = items.ratingSite)
+      : (document.getElementById("site").value = "average");
+  });
+}
+
+function saveOptions() {
   chrome.storage.sync.set(
     {
       apiKey: document.getElementById("api-key").value,
       ratingSite: document.getElementById("site").value,
     },
-    function () {
+    () => {
       // Update status to let user know options were saved.
       const status = document.getElementById("status");
       status.textContent = "Saved.";
-      setTimeout(function () {
+      setTimeout(() => {
         status.textContent = "";
       }, 750);
     }
   );
 }
 
-function restore_options() {
-  chrome.storage.sync.get(function (items) {
-    items.items.apiKey
-      ? (document.getElementById("site").value = items.ratingSite)
-      : (document.getElementById("site").value = "<Insert your OMDb API key>");
-    document.getElementById("api-key").value = items.apiKey;
-    items.ratingSite
-      ? (document.getElementById("site").value = items.ratingSite)
-      : (document.getElementById("site").value = "average");
+function sendMessage() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" });
   });
 }
